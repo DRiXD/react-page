6ca113ef1bead0540eab0bde0d1da925c3eb2521
Reibel, Denis <Denis.Reibel@dm.de>
2019-09-02 17:10:35 +0200

    added infoPane

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import ButtonPane from './Buttons/ButtonPane.js';
 import References from './references/References.js';
+import InfoPane from './infoPane/InfoPane.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <InfoPane></InfoPane>
         <img src={logo} className="App-logo" alt="logo" />
         <p> Hello World! </p>
         <ButtonPane></ButtonPane>
---
+++ src/infoPane/InfoPane.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './infoPane.css';
+
+
+function InfoPane() {
+  return (
+    <div class="infoPane">
+        <img></img>
+        <h1>Hi i'm Denis Reibel</h1>
+        <p> Lirum Larum Löffelstiel </p>
+    </div>
+  );
+}
+
+export default InfoPane;
